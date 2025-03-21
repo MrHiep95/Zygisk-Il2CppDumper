465bc6f37350606d53145c42cb27c33282d0bedd
MrHiep95 <134181813+MrHiep95@users.noreply.github.com>
2025-03-22 01:07:50 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.hoduro.guardian"
+#define GamePackageName "com.awesomepiece.castle"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
